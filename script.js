aaa7f41616d53cb45c28f12620e5b6f8d463bc77
SufyaanASC21 <sufyaaan2005@yahoo.com>
2021-08-09 13:21:24 -0400

    second

--- script.js
+++ script.js
@@ -12,16 +12,12 @@ function imgFunction(){
 document.querySelector("#game_pic").addEventListener('click',imgFunction);
 
 
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
 var navbar = document.getElementById("big_div");
 
-// Get the offset position of the navbar
 var sticky = big_div.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     big_div.classList.add("sticky")
